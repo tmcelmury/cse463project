cdd47f11ec33728466bf546fc73403499b573b80
Trent McElmury <trent.mcelmury@huskers.unl.edu>
2022-04-30 22:16:56 -0500

    forgot to update header file

--- cpp/md5home.h
+++ cpp/md5home.h
@@ -10,3 +10,4 @@ uint32_t F(uint32_t x, uint32_t y, uint32_t z);
 uint32_t G(uint32_t x, uint32_t y, uint32_t z);
 uint32_t H(uint32_t x, uint32_t y, uint32_t z);
 uint32_t I(uint32_t x, uint32_t y, uint32_t z);
+uint32_t[64] generateT();
