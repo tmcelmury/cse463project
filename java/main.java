4d791160be81e2f2bbea2f37d42b22a07baece41
Trent McElmury <trent.mcelmury@huskers.unl.edu>
2022-05-04 21:08:28 -0500

    java main updated

---
+++ java/main.java
@@ -0,0 +1,9 @@
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.util.concurrent.TimeUnit;
+//import java.security.NoSuchAlgorithmException;
+
+public static void main(String args[]) {
+
+}
